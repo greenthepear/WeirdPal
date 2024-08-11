70a29609013c5e9c262e9e4a5f389270278462bf
greenthepear <green@greenthepear.com>
2024-08-11 17:03:21 +0200

    Input of original lyrics working

--- src/Main.elm
+++ src/Main.elm
@@ -2,7 +2,7 @@ module Main exposing (..)
 
 import Browser
 import Dict exposing (Dict)
-import Html exposing (Attribute, Html, div, input, text)
+import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
 import Regex
@@ -12,6 +12,7 @@ import Regex
 -- MAIN
 
 
+main : Program () Model Msg
 main =
     Browser.sandbox { init = init, update = update, view = view }
 
@@ -35,7 +36,10 @@ type alias Model =
 init : Model
 init =
     { content =
-        Dict.fromList [ ( 0, Line 0 "I am a sinner, who's probably gonna sin again" "" ) ]
+        Dict.fromList
+            [ ( 0, Line 0 "This is a placeholder!" "" )
+            , ( 1, Line 1 "Add the original lyrics on the right" "" )
+            ]
     }
 
 
@@ -45,6 +49,8 @@ init =
 
 type Msg
     = Change Int String
+    | ChangeOriginal Int String
+    | ChangeOriginalFromMultiline String
 
 
 syllable : Regex.Regex
@@ -62,7 +68,41 @@ update : Msg -> Model -> Model
 update msg model =
     case msg of
         Change lineNum newString ->
-            { model | content = Dict.update lineNum (Maybe.map (\line -> { line | parody = newString })) model.content }
+            { model
+                | content =
+                    Dict.update lineNum
+                        (\maybeLine ->
+                            case maybeLine of
+                                Just line ->
+                                    Just { line | parody = newString }
+
+                                Nothing ->
+                                    Just { number = lineNum, parody = newString, original = "" }
+                        )
+                        model.content
+            }
+
+        ChangeOriginal lineNum newString ->
+            { model
+                | content =
+                    Dict.update lineNum
+                        (\maybeLine ->
+                            case maybeLine of
+                                Just line ->
+                                    Just { line | original = newString }
+
+                                Nothing ->
+                                    Just { number = lineNum, original = newString, parody = "" }
+                        )
+                        model.content
+            }
+
+        ChangeOriginalFromMultiline str ->
+            let
+                changes =
+                    String.lines str |> List.indexedMap (\i e -> ChangeOriginal i e)
+            in
+            List.foldl update model changes
 
 
 
@@ -71,19 +111,73 @@ update msg model =
 
 lineCountStyle : List (Attribute msg)
 lineCountStyle =
-    [ style "justify-self" "center", style "text-align" "center" ]
+    [ style "justify-self" "center"
+    , style "text-align" "center"
+    ]
+
+
+containerStyle : List (Attribute msg)
+containerStyle =
+    [ style "display" "flex"
+    , style "justify-content" "space-between"
+    , style "position" "absolute"
+    , style "width" "100%"
+    , style "height" "100%"
+    ]
+
+
+rightContainerStyle : List (Attribute msg)
+rightContainerStyle =
+    [ style "display" "flex"
+    , style "flex-direction" "column"
+    , style "justify-content" "space-between"
+    , style "width" "50%"
+    , style "height" "100%"
+    , style "border" "2px solid black"
+    ]
 
 
 makeSongLine : Line -> List (Html Msg)
 makeSongLine ln =
     [ div lineCountStyle [ text (syllableCount ln.original |> String.fromInt) ]
-    , div [] [ input [ value ln.original, disabled True, style "width" "100%" ] [] ]
+    , div [] [ input [ value ln.original, disabled True, style "width" "98%" ] [] ]
     , div lineCountStyle [ text <| String.fromInt <| syllableCount <| ln.parody ]
-    , div [] [ input [ placeholder "...", onInput (Change ln.number), style "width" "100%" ] [] ]
+    , div [] [ input [ placeholder "...", onInput (Change ln.number), style "width" "98%" ] [] ]
     ]
+
+
+makeSongLineDiv : Line -> Html Msg
+makeSongLineDiv ln =
+    div
+        [ style "display" "grid"
+        , style "grid-template-columns" "1fr 20fr"
+        , style "padding-top" "1%"
+        , style "width" "100%"
+        ]
+        (makeSongLine ln)
 
 
 view : Model -> Html Msg
 view model =
-    div [ style "display" "grid", style "grid-template-columns" "1fr 60fr" ]
-        (makeSongLine (Maybe.withDefault (Line 0 "" "") (Dict.get 0 model.content)))
+    div containerStyle
+        [ div [ style "width" "60%" ]
+            (Dict.values
+                model.content
+                |> List.map makeSongLineDiv
+            )
+        , div rightContainerStyle
+            [ div [ style "height" "100%" ]
+                [ p [] [ text "Original:" ]
+                , textarea
+                    [ onInput ChangeOriginalFromMultiline
+                    , style "width" "100%"
+                    , style "height" "50%"
+                    ]
+                    []
+                ]
+            , div [ style "height" "100%" ]
+                [ p [] [ text "Changed:" ]
+                , textarea [ style "width" "100%", style "height" "50%" ] []
+                ]
+            ]
+        ]
